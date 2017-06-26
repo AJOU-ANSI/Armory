a37c3cccdf1474cfee18e331fa78cd57bcbe4cc7
Taewoo Kim <xodn4195@gmail.com>
2017-06-27 00:12:35 +0900

    Modify code type to string

--- app/models/problem.js
+++ app/models/problem.js
@@ -10,7 +10,7 @@ module.exports = function (sequelize, DataTypes) {
       allowNull: false
     },
     code: {
-      type: DataTypes.TEXT,
+      type: DataTypes.STRING,
       allowNull: false
     }
   }, {
