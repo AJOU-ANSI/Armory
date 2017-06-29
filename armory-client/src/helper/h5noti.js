7450aee3c1d49e9e11b4c68362e600b0469953fe
Taewoo Kim <xodn4195@gmail.com>
2017-06-29 22:52:03 +0900

    Add if condition for notification in safari

--- armory-client/src/helper/h5noti.js
+++ armory-client/src/helper/h5noti.js
@@ -5,26 +5,32 @@ const obj = {};
 export default obj;
 
 obj.init = () => {
-  Notification.requestPermission(function (result) {
-    if (result !== 'denied') {
-      activate = true;
-    }
-  });
+  if (window.Notification) {
+    window.Notification.requestPermission(function (result) {
+      if (result !== 'denied') {
+        activate = true;
+      }
+    });
+  }
+
 };
 
 obj.sendMessage = (message) => {
-  if (activate) {
-    const options = {
-      body: message
-    };
+  if (window.Notification) {
+    if (activate) {
+      const options = {
+        body: message
+      };
 
-    //데스크탑 알림 요청
-    const notification = new Notification("shake17 서비스 메세지", options);
+      //데스크탑 알림 요청
+      const notification = new Notification("shake17 서비스 메세지", options);
 
-    //알림 후 5초 뒤,
-    setTimeout(function () {
-      //얼람 메시지 닫기
-      notification.close();
-    }, 5000);
+      //알림 후 5초 뒤,
+      setTimeout(function () {
+        //얼람 메시지 닫기
+        notification.close();
+      }, 5000);
+    }
   }
+
 };
