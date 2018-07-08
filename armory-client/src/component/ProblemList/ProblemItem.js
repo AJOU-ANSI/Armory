d81969793c8d5b64d09925b4e495db84f3f3485a
Taewoo Kim <xodn4195@gmail.com>
2018-07-08 11:14:13 +0900

    feature: add bg-color in problem list

--- armory-client/src/component/ProblemList/ProblemItem.js
+++ armory-client/src/component/ProblemList/ProblemItem.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 import './ProblemItem.css';
 
 export const ProblemItem = ({problem, className, match, status}) => {
-  let btnClass, btnLabel;
+  let btnClass, btnLabel, btnBg;
 
   if (!status) {
     btnClass = 'btn-white text-logo'; btnLabel = '문제 풀기';
@@ -14,10 +14,10 @@ export const ProblemItem = ({problem, className, match, status}) => {
     const {accepted, wrong} = status;
 
     if (accepted) {
-      btnClass = 'btn-info'; btnLabel = '정답';
+      btnClass = 'btn-info'; btnLabel = '정답'; btnBg = '#E3F2FD';
     }
     else if (wrong) {
-      btnClass = 'btn-danger'; btnLabel = '재시도';
+      btnClass = 'btn-danger'; btnLabel = '재시도'; btnBg = '#FFEBEE';
     }
     else {
       btnClass = 'btn-white text-logo'; btnLabel = '문제 풀기';
@@ -27,7 +27,7 @@ export const ProblemItem = ({problem, className, match, status}) => {
   return (
     <div className={classnames('ProblemItem', className)}>
       <div className="card problem-card paper">
-        <div className="card-block">
+        <div className="card-block" style={{backgroundColor: btnBg}}>
           <h5 className="d-flex mb-0">
             <strong>문제 {problem.code}</strong>
             <span className="ml-2 font-weight-light">{problem.title}</span>
